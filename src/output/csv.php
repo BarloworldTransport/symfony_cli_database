d9fbf87cf68135fb922864d4fadd963605832da8
www-data <www-data@bunsen.bwt.local>
2017-12-13 13:58:10 +0200

    csv output added

--- src/output/csv.php
+++ src/output/csv.php
@@ -6,6 +6,24 @@ class csv implements output
 {
 	public function output(array $data, string $outputLocation = '/tmp/data.csv')
 	{
-
+		if (is_string($outputLocation) === false) {
+            syslog(LOG_INFO, "Cannot write to file. Path given isn't a string");
+            return false;
+        }
+        $ptr = fopen($outputLocation, 'w+');
+        if ($ptr === false) {
+			syslog(LOG_INFO, 'Unable to open file: ' . $outputLocation . ' for writing.');
+			return false;
+		}
+        $int = (int) 0;
+        foreach ($data as $val) {
+            if ($int === 0) {
+                fputcsv($ptr, array_keys($val));
+            }
+            fputcsv($ptr, $val);
+            $int ++;
+        }
+        fclose($ptr);
+        return true;
 	}
 }
