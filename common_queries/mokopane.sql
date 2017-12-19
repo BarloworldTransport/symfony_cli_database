4aeebba89b790c82c4033b2bb7095c062d88a041
www-data <www-data@bunsen.bwt.local>
2017-12-19 11:20:16 +0200

    Fixing broken parameter substition: '?' versus ?

--- common_queries/mokopane.sql
+++ common_queries/mokopane.sql
@@ -1,4 +1,4 @@
 SELECT * 
 FROM `mobilizesystem`.`login_history` 
-WHERE `name` IN ('jmatjiu@bwtrans.co.za','ghlabe@bwtrans.co.za','mmokgokolo@bwtrans.co.za') AND `time`>='?' AND `time`<='?'
+WHERE `name` IN ('jmatjiu@bwtrans.co.za','ghlabe@bwtrans.co.za','mmokgokolo@bwtrans.co.za') AND `time`>=? AND `time`<=?
 ORDER BY `name` ASC, `ID` ASC;
