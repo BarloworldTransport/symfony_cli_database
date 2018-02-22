1b8243e8033637083ea1a86b3253a5b1d3403ebc
Feighen Oosterbroek <feighen@gmail.com>
2018-02-22 12:29:49 +0200

    Commit by Rishad. The time added was 2 minutes - should be 2 hours

--- common_queries/trackmatic_trips.sql
+++ common_queries/trackmatic_trips.sql
@@ -1,22 +1,22 @@
 SELECT `udo_cargo`.`ID` AS `Cargo ID`, `udo_cargo`.`tripNumber` AS `Trip Number`, `udo_customer`.`tradingName` AS `Customer`, 
 CONCAT(`udo_driver`.`nickname`, ' [', `udo_driver`.`staffNumber`, ']') AS `Driver`, `udo_truck`.`fleetnum` AS `Truck`,
-`udo_fleet`.`name` AS `Primary Fleet`, FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`loadingArrivalETA`)+120) AS `Loading Arrival ETA`, 
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`loadingArrivalTime`)+120) AS `LoadingArrival Time`, 
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`loadingStarted`)+120) AS `Loading Started`, 
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`loadingFinished`)+120) AS `Loading Finished`, 
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`timeLeft`)+120) AS `Time Left`, 
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`offloadingArrivalETA`)+120) AS `Loading Arrival ETA`, 
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`offloadingArrivalTime`)+120) AS `Offloading Arrival Time`, 
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`offloadingStarted`)+120) AS `Offloading Started`, 
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`offloadingCompleted`)+120) AS `Offloading Completed`,
+`udo_fleet`.`name` AS `Primary Fleet`, FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`loadingArrivalETA`)+7200) AS `Loading Arrival ETA`, 
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`loadingArrivalTime`)+7200) AS `LoadingArrival Time`, 
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`loadingStarted`)+7200) AS `Loading Started`, 
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`loadingFinished`)+7200) AS `Loading Finished`, 
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`timeLeft`)+7200) AS `Time Left`, 
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`offloadingArrivalETA`)+7200) AS `Loading Arrival ETA`, 
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`offloadingArrivalTime`)+7200) AS `Offloading Arrival Time`, 
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`offloadingStarted`)+7200) AS `Offloading Started`, 
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`offloadingCompleted`)+7200) AS `Offloading Completed`,
 `fpp`.`name` AS `Loading Town`, `fpl`.`name` AS `Loading Point`,`tpp`.`name` AS `Offloading Town`, `tpl`.`name` AS `Offloading Point`,
 `udo_tripleg`.`kmsBegin` AS `Kms Begin`,`udo_tripleg`.`kmsEnd` AS `Kms End`,`udo_odo`.`odo` AS `Depot Odo`, 
-`udo_cargo`.`tonsActual` AS `Tons Actual`, FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`time_created`)+120) AS `Time Created`,
+`udo_cargo`.`tonsActual` AS `Tons Actual`, FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`time_created`)+7200) AS `Time Created`,
 IF(!ISNULL(`cp`.`first_name`), CONCAT(`cp`.`first_name`, ' ', `cp`.`last_name`), `cp`.`email`) AS `Created By`,
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`time_last_modified`)+120) AS `Last Update Time`,
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_tripleg`.`time_last_modified`)+7200) AS `Last Update Time`,
 IF(!ISNULL(`lmp`.`first_name`), CONCAT(`lmp`.`first_name`, ' ', `lmp`.`last_name`), `lmp`.`email`) AS `Last Updated By`,
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_trackingload`.`requestSent`)+120) AS `Trackmatic Request Sent`,
-FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_trackingload`.`activated`)+120) AS `Driver Activated On`
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_trackingload`.`requestSent`)+7200) AS `Trackmatic Request Sent`,
+FROM_UNIXTIME(UNIX_TIMESTAMP(`udo_trackingload`.`activated`)+7200) AS `Driver Activated On`
 FROM `udo_cargo`
 INNER JOIN `udo_trackingload` ON `udo_cargo`.`trip_id`=`udo_trackingload`.`trip_id`
 LEFT JOIN `udo_triplegcargo` ON `udo_cargo`.`ID`=`udo_triplegcargo`.`cargo_id`
@@ -37,4 +37,4 @@ LEFT JOIN `permissionuser` AS `cb` ON `cb`.`id`=`udo_tripleg`.`created_by`
 LEFT JOIN `person` AS `cp` ON `cp`.`ID`=`cb`.`person_id`
 LEFT JOIN `permissionuser` AS `lmb` ON `lmb`.`id`=`udo_tripleg`.`last_modified_by`
 LEFT JOIN `person` AS `lmp` ON `lmp`.`ID`=`lmb`.`person_id`
-WHERE IF(!ISNULL(`udo_tripleg`.`loadingArrivalTime`),`udo_tripleg`.`loadingArrivalTime`,`udo_tripleg`.`loadingArrivalETA`)>='2017-09-30 22:00:00'
+WHERE IF(!ISNULL(`udo_tripleg`.`loadingArrivalTime`),`udo_tripleg`.`loadingArrivalTime`,`udo_tripleg`.`loadingArrivalETA`)>='2018-01-01 00:00:00'
